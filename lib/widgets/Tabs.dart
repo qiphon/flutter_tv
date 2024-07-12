f76a90a991afdd44ae1469f527a379a5f34b04be
qiphon <qiphon@zoedeMac-mini.local>
2024-07-12 11:32:25 +0800

    chore tab component finish

--- lib/widgets/Tabs.dart
+++ lib/widgets/Tabs.dart
@@ -1,8 +1,10 @@
-import 'dart:developer';
+///
+/// @file 自定义 tab ，暂时只做 tab 顶部切换的样式
+///
+///
+library;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 enum TabPosition {
   top,
@@ -19,10 +21,16 @@ class TabItem {
   });
 }
 
+typedef OnTabChange = void Function(int index);
+
 class Tabs extends StatefulWidget {
   final Widget pages;
+  final int activeTab;
+  final double iconSize;
   final List<TabItem> tabs;
+  final Color tabActiveColor;
   final TabPosition tabPosition;
+  final OnTabChange? onTabChange;
 
   /// tab 之间的间距
   final double tabSpace;
@@ -32,7 +40,11 @@ class Tabs extends StatefulWidget {
       {super.key,
       required this.pages,
       required this.tabs,
+      this.onTabChange,
       this.tabSpace = 8,
+      this.activeTab = 0,
+      this.iconSize = 20,
+      this.tabActiveColor = Colors.blue,
       this.padding = const EdgeInsets.all(16),
       this.tabPosition = TabPosition.top});
 
@@ -44,8 +56,22 @@ class Tabs extends StatefulWidget {
 
 class _TabsState extends State<Tabs> {
   int _currentTab = 0;
+  Color primaryColor = Colors.blue;
+
+  // dart 无法监听父级数据变化同步到当前值
+  // @override
+  // void didUpdateWidget(covariant Tabs oldWidget) {
+  //   // TODO: implement didUpdateWidget
+  //   super.didUpdateWidget(oldWidget);
+  //   // if (widget.activeTab != _currentTab) {
+  //   // _onTabChange(widget.activeTab);
+  //   // }
+  // }
 
   void _onTabChange(int value) {
+    if (widget.onTabChange != null) {
+      widget.onTabChange!(value);
+    }
     setState(() {
       _currentTab = value;
     });
@@ -54,13 +80,15 @@ class _TabsState extends State<Tabs> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromARGB(1, 234, 167, 167),
       child: _runderTabWithPosition(),
     );
   }
 
-  Widget _renderText(String text) {
-    return Text(style: const TextStyle(fontSize: 24), text);
+  Widget _renderText(String text, int index) {
+    return Text(
+        style: TextStyle(
+            fontSize: 24, color: _currentTab == index ? primaryColor : null),
+        text);
   }
 
   List<Widget> _renderTabItemInner() {
@@ -69,16 +97,20 @@ class _TabsState extends State<Tabs> {
     for (int index = 0; index < itemLen; index++) {
       final item = widget.tabs[index];
       result.add(Container(
-        child: widget.tabPosition == TabPosition.top
-            ? Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Icon(item.icon), _renderText(item.title)],
-              )
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [Icon(item.icon), _renderText(item.title)],
-              ),
-      ));
+          child: GestureDetector(
+              onTap: () => _onTabChange(index),
+              child: Flex(
+                direction: widget.tabPosition == TabPosition.top
+                    ? Axis.vertical
+                    : Axis.horizontal,
+                children: [
+                  Icon(
+                      size: widget.iconSize,
+                      color: _currentTab == index ? primaryColor : null,
+                      item.icon),
+                  _renderText(item.title, index)
+                ],
+              ))));
       if (index < itemLen - 1) {
         result.add(SizedBox(
           width: widget.tabSpace,
@@ -107,14 +139,7 @@ class _TabsState extends State<Tabs> {
             ? Column(
                 children: [
                   Flexible(flex: 0, child: _renderTabItem()),
-                  // widget.pages
-                  Expanded(
-                      child: Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    child: widget.pages,
-                    // child: Text('sdfsdfsd'),
-                  ))
+                  widget.pages,
                 ],
               )
             : Row(children: [
